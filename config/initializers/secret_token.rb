1e895eb401644482add1596b8c8db7d9d3711e80
vid <vidya.thangirala@gmail.com>
2014-02-12 11:13:23 +0800

    Commit all except test

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+BackgroundProcessMonitor::Application.config.secret_token = 'ab9112df9a0295dd8426556190ab9a31093a5187ea2493404d1451862868015346a1c887ad021c1abed8883a54e46aef316d4a0745155c5571450f09cf297259'
